71daf92cbcdc17f316e44867ad751238ac34dcb6
jaeme <jaemeong3131@gmail.com>
2024-12-07 14:10:30 +0900

    test

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:witibju/screens/home/models/main_view_model.dart';
 import 'package:witibju/screens/home/widgets/wit_home_widgets2.dart';
 import 'package:witibju/screens/home/wit_home_sc.dart';
 import 'package:witibju/screens/home/wit_kakaoLogin.dart';
+import 'package:witibju/screens/home/wit_kakaoLogin_home_sc.dart';
 import 'package:witibju/screens/home/wit_navigation_home_sc.dart'; // NavigationHomeScreen 임포트
 import 'package:witibju/screens/preInspaction/wit_preInsp_main_sc.dart';
 import 'package:witibju/screens/question/wit_question_main_sc.dart';
@@ -17,7 +18,7 @@ void main() {
   WidgetsFlutterBinding.ensureInitialized();
 
   KakaoSdk.init(
-    nativeAppKey: '7f8159a6844612331340490d87cbb0bb',
+    nativeAppKey: '25cc33cc258862ad87987baa7b5f4477',
   );
 
   runApp(
@@ -38,8 +39,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       ///home: NavigationHomeScreen(),  기존 밀어서
-      home: HomeScreen(), //현재 메인
+    home: HomeScreen(), //현재 메인
       //home: PreInspaction(),
+
       ///home: ImageSlider(),
       //home: Board("B01"),  //게시판
       ///home: Question(qustCd: 'Q00001'),  // 질의문
