b68db794b343d22b7357b9c154d557a5d833b691
gram <jjangart5720@naver.com>
2025-03-15 13:40:05 +0900

    체크리스트 변경 2025.03.15
    Signed-off-by: gram <jjangart5720@naver.com>

--- lib/screens/checkList/widget/wit_checkList_detail_widget.dart
+++ lib/screens/checkList/widget/wit_checkList_detail_widget.dart
@@ -215,7 +215,7 @@ class ExpandableItem extends StatelessWidget {
                           isScrollControlled: true, // 스크롤 가능하게 설정
                           builder: (context) {
                             return Container(
-                              height: MediaQuery.of(context).size.height * 0.56,
+                              height: 500,
                               child: ExamplePhotoPopup(
                                 checkInfoLv3: checkInfoLv3,
                                 onSwitchChanged: onSwitchChanged,
