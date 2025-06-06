984549aba49fc7311d5519a1736feacb198b4c65
baek <dravenn83@gmail.com>
2025-06-06 14:33:04 +0900

    견적리스트에서 판매자 번호 제거 세션 처리
    Signed-off-by: baek <dravenn83@gmail.com>

--- lib/screens/seller/wit_seller_profile_appbar_sc.dart
+++ lib/screens/seller/wit_seller_profile_appbar_sc.dart
@@ -73,7 +73,7 @@ class SellerAppBarState extends State<SellerAppBar> with RouteAware {
 
   Future<void> initAsync() async {
     if (sllrNo == null) {
-      await secureStorage.write(key: 'sllrNo', value: "241");
+      // await secureStorage.write(key: 'sllrNo', value: "");
       sllrNo = await secureStorage.read(key: 'sllrNo');
       print("sllrNo from secureStorage ::: $sllrNo");
     }
